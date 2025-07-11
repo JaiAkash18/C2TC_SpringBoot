8e29347f710565913bcde205bc3e9b290f838eff
JaiAkash18 <akashjai1505@gmail.com>
2025-07-11 12:27:34 +0530

    Delete CourseModule directory

--- CourseModule/src/main/java/com/tnsif/CourseModule/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnsif.CourseModule.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tnsif.CourseModule.entity.Course;
-
-public interface CourseRepository extends JpaRepository<Course, Long>{
-
-}
